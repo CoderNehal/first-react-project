9e242d666ecd7b223cace3f296ddfeade70f2f84
CoderNehal <nehalughade1221@gmail.com>
2021-05-25 20:59:32 +0530

    Added user Log In UI

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import './User.css'
+import backbtn from '../../images/user-Left-arrow.svg'
 const User = () => {
     return (
-        <div>
-            <h1>This is user aye!!!!!!!</h1>
+        <div className="UserContainer">
+            <div className="User">
+                <div className="userNav">
+                    <img src={backbtn} alt="" />
+                   
+                </div>
+                <h3>Login</h3>
+                <br />
+                <br />
+                <p>Email</p><input type="text" name="" id="" /><br /><br />
+                <p>Password</p><input type="password" name="" id="" />
+                <br /><br /><br />
+                <div className="submit">
+                <input type="button" value='Log in' name="" id="" />
+                </div>
+               
+            </div>
         </div>
+        
     )
 }
 
